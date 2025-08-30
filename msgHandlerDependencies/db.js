fa387e2674780cb86c058a2bbfd26d846960793c
Vlad-Linnik <linnikv10@gmail.com>
2025-08-30 13:22:07 +0300

    ok

--- msgHandlerDependencies/db.js
+++ msgHandlerDependencies/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
 
 let db;
