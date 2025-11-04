953962957e75412d58eab0609d2444cf6ccb8df5
Vlad Linnik <linnikv10@gmail.com>
2025-11-04 18:39:54 +0100

    non tmi token removed

--- botInitInfo.js
+++ botInitInfo.js
@@ -14,7 +14,6 @@ const botInitInfo = {
   "bot_id": process.env.bot_id,
   "OAUTHtoken": process.env.OAUTHtoken,
   "Client_Id": process.env.Client_Id,
-  "password_Not_TMI_Token": process.env.password_Not_TMI_Token,
   "channels": [channel],
   "version": "0.1.1",
   "startTime": startTime
