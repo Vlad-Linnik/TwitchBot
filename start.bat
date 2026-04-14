725b406e703a2ccae54429f863a7c095177b8f99
Vlad-Linnik <linnikv10@gmail.com>
2026-04-14 15:23:44 +0200

    refactor

--- start.bat
+++ start.bat
@@ -1,6 +1,12 @@
 @echo off
 taskkill /F /IM node.exe >nul 2>&1
-git pull
-call npm install
-call npm audit fix
+
+echo Pulling updates...
+git fetch --all
+git reset --hard origin/main
+
+echo Installing dependencies...
+call npm ci
+
+echo Starting app...
 start /B "" powershell -WindowStyle Hidden -Command "node .\index.js"
